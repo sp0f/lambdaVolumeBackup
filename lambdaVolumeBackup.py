fff06a7f639e73f9dd854d2e188169d941d68702
sp0f <adam.lyjak@gmail.com>
2017-09-27 14:35:25 +0200

    improved filtering

--- lambdaVolumeBackup.py
+++ lambdaVolumeBackup.py
@@ -4,58 +4,40 @@ import boto3
 import datetime
 import pytz
 
-def lambda_handler(event,context):
-    ec2 = boto3.resource('ec2')
-    retention_days = 2
+#def lambda_handler(event,context):
+ec2 = boto3.resource('ec2')
+retention_days = 2
 
-    instances = ec2.instances.filter(Filters=[
-        {
-            'Name': 'instance-state-name',
-            'Values': ['running']
-        }
-    ])
+for volume in ec2.volumes.filter(Filters=[{'Name': 'tag:lambdaVolumeBackup', 'Values': ['true']}]):
+    description = "lambdaVolumeBackup-" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + "-" + volume.volume_id
 
-    for instance in instances:
-        # print filter(lambda tag: tag['Key'] == 'Name', instance.tags)
-        instance_name = filter(lambda tag: tag['Key'] == 'Name', instance.tags)[0]['Value']
+    # create snapshot
 
-        for volume in ec2.volumes.filter(Filters=[{'Name': 'attachment.instance-id', 'Values': [instance.id]}]):
-            if volume.tags:
-                result = filter(lambda tag: tag['Key'] == 'lambdaVolumeBackup', volume.tags)
-                if len(result) != 0:
-                    if (result[0]['Value']) == "true":
-                        description = "lambdaVolumeBackup-" + datetime.datetime.now().strftime(
-                            "%Y%m%d-%H%M%S") + "-" + instance_name + "-" + volume.volume_id
+    snapshot = volume.create_snapshot(volume.volume_id, Description=description)
 
-                        # create snapshot
+    if snapshot:
+        print "Snapshot: " + str(snapshot.id) + " created successfully"
+        snapshot.create_tags(
+            Tags=[
+                {
+                    'Key': 'archive',
+                    'Value': 'true'
+                }
+            ]
+        )
 
-                        snapshot = volume.create_snapshot(volume.volume_id, Description=description)
-
-                        if snapshot:
-                            print "Snapshot: " + str(snapshot.id) + " created sucessfully"
-                            snapshot.create_tags(
-                                Tags=[
-                                    {
-                                        'Key': 'archive',
-                                        'Value': 'true'
-                                    }
-                                ]
-                            )
-
-                        # retention
-                        for snapshot in volume.snapshots.all():
-                            '''
-                            if snapshot.description.startswith('lambdaVolumeBackup-'):
-                                print snapshot.id
-                                print str(datetime.datetime.now().replace(tzinfo=None) - snapshot.start_time.replace(tzinfo=None))+" > "\
-                                    + str(datetime.timedelta(days=retention_days)) + " = " \
-                                    + str((datetime.datetime.now().replace(tzinfo=None) - snapshot.start_time.replace(tzinfo=None)) > datetime.timedelta(days=retention_days))
-                            '''
-
-                            if snapshot.description.startswith('lambdaVolumeBackup-'):
-                                if (datetime.datetime.now().replace(tzinfo=None) - snapshot.start_time.replace(
-                                        tzinfo=None)) > datetime.timedelta(days=retention_days):
-                                    print("\t\tDeleting snapshot [%s - %s]" % (snapshot.id, snapshot.description))
-                                    snapshot.delete()
-    print("lambdaVolumeBackup completed!")
-    return True
+    for snapshot in volume.snapshots.all():
+        if snapshot.description.startswith('lambdaVolumeBackup-'):
+            print snapshot.id
+            print str(datetime.datetime.now().replace(tzinfo=None) - snapshot.start_time.replace(tzinfo=None))+" > "\
+                + str(datetime.timedelta(days=retention_days)) + " = " \
+                + str((datetime.datetime.now().replace(tzinfo=None) - snapshot.start_time.replace(tzinfo=None)) > datetime.timedelta(days=retention_days))
+        '''
+        if snapshot.description.startswith('lambdaVolumeBackup-'):
+            if (datetime.datetime.now().replace(tzinfo=None) - snapshot.start_time.replace(
+                    tzinfo=None)) > datetime.timedelta(days=retention_days):
+                print("\t\tDeleting snapshot [%s - %s]" % (snapshot.id, snapshot.description))
+                snapshot.delete()
+        '''
+#    print("lambdaVolumeBackup completed!")
+#    return True
--- lambdaVolumeBackup_check.py
+++ lambdaVolumeBackup_check.py
@@ -6,15 +6,13 @@ volume_list=[]
 
 for volume in ec2.volumes.filter(Filters=[{'Name': 'tag:lambdaVolumeBackup', 'Values': ['true']}]):
     if volume.tags:
-        result = filter(lambda tag: tag['Key'] == 'lambdaVolumeBackup', volume.tags)
-        if len(result) != 0:
-            if (result[0]['Value']) == "true":
-                volume_snapshots=volume.snapshots.all()
-                snapshotCount=0
-                for snapshot in volume_snapshots:
-                    if snapshot.description.startswith('lambdaVolumeBackup-'):
-                        snapshotCount=+1
-                if snapshotCount < retention_days:
+        volume_snapshots=volume.snapshots.all()
+        snapshotCount=0
+        for snapshot in volume_snapshots:
+            if snapshot.description.startswith('lambdaVolumeBackup-'):
+                snapshotCount=snapshotCount+1
+                print snapshot.id+"("+str(snapshotCount)+")"
+        if snapshotCount < retention_days:
                     volume_list.append(volume.id+"("+str(snapshotCount)+")")
 
 if len(volume_list) != 0:
